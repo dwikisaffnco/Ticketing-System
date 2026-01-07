d5bb33d9554833272ffd606ba7b260e295fbf367
wikidotexe <07wikiarlianm@gmail.com>
2026-01-07 15:24:11 +0700

    fix file system

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -47,6 +47,15 @@ return [
             'report' => false,
         ],
 
+        'attachments' => [
+            'driver' => 'local',
+            'root' => storage_path('app/attachments'),
+            'url' => env('APP_URL').'/storage/attachments',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
